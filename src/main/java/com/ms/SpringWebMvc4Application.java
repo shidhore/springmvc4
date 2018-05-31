f5d44a1bc843f490d9bef8b0f0b6ff5004848915
Mandar.Shidhore <mandar.shidhore@travelport.com>
2018-05-31 06:41:46 -0600

    Initial Commit

---
+++ src/main/java/com/ms/SpringWebMvc4Application.java
@@ -0,0 +1,12 @@
+package com.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvc4Application.class, args);
+	}
+}
